ca0946b655b96d2331083178eaa6e9b88611679d
MarynaGL <maryna.glazkina@gmail.com>
2026-04-22 16:43:13 -0300

    Cambio de puerto

--- server.js
+++ server.js
@@ -67,7 +67,8 @@ app.get('/resultados', async (req, res) => {
     }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
